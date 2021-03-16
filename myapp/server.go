d91673135c3d09e2826f86d2fe7a251f3ae4a207
nero-15 <ti7805@gmail.com>
2021-03-16 22:09:32 +0900

    bind param

--- myapp/server.go
+++ myapp/server.go
@@ -6,6 +6,11 @@ import (
 	echo "github.com/labstack/echo/v4"
 )
 
+type User struct {
+	Name  string `json:"name" param:"name" query:"name"`
+	Email string `json:"email" param:"email" query:"email"`
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
@@ -22,5 +27,14 @@ func main() {
 		member := c.QueryParam("member")
 		return c.String(http.StatusOK, "team:"+team+", member:"+member)
 	})
+
+	e.GET("/bind/users/:name/:email", func(c echo.Context) error {
+		u := new(User)
+		if err := c.Bind(u); err != nil {
+			// error handling
+		}
+		return c.JSON(http.StatusOK, u)
+	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
