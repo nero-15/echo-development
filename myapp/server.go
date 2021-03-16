3fdcca8a617268f8d860a3afa1a1cd3a596534e9
nero-15 <ti7805@gmail.com>
2021-03-16 22:31:39 +0900

    cookie

--- myapp/server.go
+++ myapp/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	echo "github.com/labstack/echo/v4"
 )
@@ -64,5 +66,32 @@ func main() {
 		return cc.String(200, "OK")
 	})
 
+	e.GET("/writeCookie", func(c echo.Context) error {
+		return writeCookie(c)
+	})
+
+	e.GET("/readCookie", func(c echo.Context) error {
+		return readCookie(c)
+	})
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func writeCookie(c echo.Context) error {
+	cookie := new(http.Cookie)
+	cookie.Name = "username"
+	cookie.Value = "jon"
+	cookie.Expires = time.Now().Add(24 * time.Hour)
+	c.SetCookie(cookie)
+	return c.String(http.StatusOK, "write a cookie")
+}
+
+func readCookie(c echo.Context) error {
+	cookie, err := c.Cookie("username")
+	if err != nil {
+		return err
+	}
+	fmt.Println(cookie.Name)
+	fmt.Println(cookie.Value)
+	return c.String(http.StatusOK, "read a cookie")
 }
