8a7afa58cf02b114bb18a746acd9a900910ebb9f
nero-15 <ti7805@gmail.com>
2021-03-16 22:23:52 +0900

    custom context

--- myapp/server.go
+++ myapp/server.go
@@ -11,8 +11,29 @@ type User struct {
 	Email string `json:"email" param:"email" query:"email"`
 }
 
+type CustomContext struct {
+	echo.Context
+}
+
+func (c *CustomContext) Foo() {
+	println("foo")
+}
+
+func (c *CustomContext) Bar() {
+	println("bar")
+}
+
 func main() {
 	e := echo.New()
+
+	// custom context
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			cc := &CustomContext{c}
+			return next(cc)
+		}
+	})
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
@@ -34,6 +55,13 @@ func main() {
 			// error handling
 		}
 		return c.JSON(http.StatusOK, u)
+	})
+
+	e.GET("/customContext", func(c echo.Context) error {
+		cc := c.(*CustomContext)
+		cc.Foo()
+		cc.Bar()
+		return cc.String(200, "OK")
 	})
 
 	e.Logger.Fatal(e.Start(":1323"))
